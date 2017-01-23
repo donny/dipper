7d1e3b1867ef35d0da6112c9bb50e1ec18003dea
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-01-24 08:07:29 +1100

    Refactor remote code

---
+++ src/Remote.js
@@ -0,0 +1,31 @@
+const address = 'http://fiftytwo-cors-anywhere.herokuapp.com/swapi.co/api'
+
+function get(query, cb) {
+    return fetch(address + query, {
+            accept: 'application/json'
+        }).then(checkStatus)
+        .then(parseJSON)
+        .then(cb);
+}
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        const error = new Error(`HTTP Error ${response.statusText}`);
+        error.status = response.statusText;
+        error.response = response;
+        console.log(error);
+        throw error;
+    }
+}
+
+function parseJSON(response) {
+    return response.json();
+}
+
+const Remote = {
+    get
+};
+
+export default Remote;
