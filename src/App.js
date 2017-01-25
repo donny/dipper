4fe18491732d418c332d8ae79d92f1c7a4b719d1
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-01-25 23:13:53 +1100

    Add pulsating logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import Gallery from './Gallery'
 
@@ -9,11 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Gallery/>
       </div>
     );
